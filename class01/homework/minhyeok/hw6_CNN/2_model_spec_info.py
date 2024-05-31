a116f26da322f29f8aaa5c090f696e5d2eed2f46
min-hyeok-Park <mhpark1114@gmail.com>
2024-05-31 15:06:00 +0900

    hw6_CNN

---
+++ class01/homework/minhyeok/hw6_CNN/2_model_spec_info.py
@@ -0,0 +1,29 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+historyNoBatch = pickle.load(open('./hw6_CNN/historyNoBatch', "rb"))
+historyBatch = pickle.load(open('./hw6_CNN/historyBatch', "rb"))
+historylelu = pickle.load(open('./hw6_CNN/historyBatchReLu', "rb"))
+val_accNB = historyNoBatch["val_accuracy"]
+val_lossNB= historyNoBatch["val_loss"]
+val_lossB = historyBatch["val_loss"]
+val_accB = historyBatch["val_accuracy"]
+val_lossL = historylelu["val_loss"]
+val_accL = historylelu["val_accuracy"]
+
+plt.subplot(1,2,1)
+plt.title('Validation Loss')
+plt.plot(range(len(val_lossNB)),val_lossNB,label = "sigmoid (No Batch)")
+plt.plot(range(len(val_lossB)),val_lossB,label = "sigmoid (Batch)")
+plt.plot(range(len(val_lossL)),val_lossL,label = "relu")
+plt.grid()
+plt.legend()
+plt.subplot(1,2,2)
+plt.title('Validation Accuracy')
+plt.plot(range(len(val_accNB)),val_accNB,label = "sigmoid (No Batch)")
+plt.plot(range(len(val_accB)),val_accB,label = "sigmoid (Batch)")
+plt.plot(range(len(val_accL)),val_accL,label = "relu")
+plt.grid()
+plt.legend()
+plt.show()
+plt.savefig("Summary.png")
