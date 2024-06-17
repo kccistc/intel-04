4f114a4307742c42365800b491f41f2274732370
eolgaemi <yjwlm10@daum.net>
2024-06-17 17:46:09 +0900

    hw3,final done

--- class02/homework/yj_jung/final/factory.py
+++ class02/homework/yj_jung/final/factory.py
@@ -1,3 +1,6 @@
+'''
+Smart Factory HW Final
+'''
 #!/usr/bin/env python3
 
 import os
@@ -9,7 +12,6 @@ from time import sleep
 import cv2
 import numpy as np
 from openvino.inference_engine import IECore
-import openvino
 
 from iotdemo import FactoryController
 from iotdemo.motion import MotionDetector
@@ -19,10 +21,9 @@ FORCE_STOP = False
 
 
 def thread_cam1(q):
-    # TODO: MotionDetector
+    """Thread function for Camera 1"""
     md = MotionDetector()
     md.load_preset()
-    # TODO: Load and initialize OpenVINO
 
     ie = IECore()
 
@@ -30,16 +31,10 @@ def thread_cam1(q):
     model_bin = "resources/model.bin"
 
     net = ie.read_network(model=model_xml, weights=model_bin)
-
     exec_net = ie.load_network(network=net, device_name="CPU")
 
-    # 입력 레이어 이름 추출
     input_blob = next(iter(net.input_info))
-
-    # 출력 레이어 이름 추출
     output_blob = next(iter(net.outputs))
-    
-    
 
     cap = cv2.VideoCapture('resources/conveyor.mp4')
 
@@ -50,51 +45,42 @@ def thread_cam1(q):
             break
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        q.put(("VIDEO:Cam1 live", frame))
 
-        q.put(("VIDEO:Cam1 live",frame))
-
-        # TODO: Motion detect
         detected = md.detect(frame)
         if detected is None:
             continue
 
-        # TODO: Enqueue "VIDEO:Cam1 detected", detected info.
-        q.put(("VIDEO:Cam1 detected",detected))
-    
-        reshaped = detected[:, :, [2, 1, 0]]
+        q.put(("VIDEO:Cam1 detected", detected))
 
+        reshaped = detected[:, :, [2, 1, 0]]
         np_data = np.moveaxis(reshaped, -1, 0)
         preprocessed_numpy = [((np_data / 255.0) - 0.5) * 2]
         batch_tensor = np.stack(preprocessed_numpy, axis=0)
 
-        # TODO: Inference OpenVINO
         result = exec_net.infer(inputs={input_blob: batch_tensor})
         output_data = result[output_blob]
         x_ratio = output_data[0][0]
         circle_ratio = output_data[0][1]
 
-        # TODO: Calculate ratios
         print(f"X = {x_ratio:.2f}%, Circle = {circle_ratio:.2f}%")
 
-        # TODO: in queue for moving the actuator 1
         if x_ratio > circle_ratio:
-            q.put(("PUSH",1))
+            q.put(("PUSH", 1))
 
     cap.release()
     q.put(('DONE', None))
-    exit()
+    os._exit(0)
 
 
 def thread_cam2(q):
-    # TODO: MotionDetector
+    """Thread function for Camera 2"""
     md = MotionDetector()
     md.load_preset()
 
-    # TODO: ColorDetector
     cd = ColorDetector()
     cd.load_preset()
 
-    # TODO: HW2 Open "resources/conveyor.mp4" video clip
     cap = cv2.VideoCapture('resources/conveyor.mp4')
 
     while not FORCE_STOP:
@@ -103,36 +89,30 @@ def thread_cam2(q):
         if frame is None:
             break
 
-        # TODO: HW2 Enqueue "VIDEO:Cam2 live", frame info
-        q.put(("VIDEO:Cam2 live",frame))
+        q.put(("VIDEO:Cam2 live", frame))
 
-        # TODO: Detect motion
         detected = md.detect(frame)
-
         if detected is None:
             continue
 
-        # TODO: Enqueue "VIDEO:Cam2 detected", detected info.
-        q.put(("VIDEO:Cam2 detected",detected))
+        q.put(("VIDEO:Cam2 detected", detected))
 
-        # TODO: Detect color
         predict = cd.detect(detected)
-        name,ratio = predict[0]
+        name, ratio = predict[0]
         ratio = ratio * 100
 
-        # TODO: Compute ratio
         print(f"{name}: {ratio:.2f}%")
 
-        # TODO: Enqueue to handle actuator 2
         if name == 'blue':
-            q.put(("PUSH",2))
+            q.put(("PUSH", 2))
 
     cap.release()
     q.put(('DONE', None))
-    exit()
+    os._exit(0)
 
 
 def imshow(title, frame, pos=None):
+    """Helper function to show image frames"""
     cv2.namedWindow(title)
     if pos:
         cv2.moveWindow(title, pos[0], pos[1])
@@ -140,6 +120,7 @@ def imshow(title, frame, pos=None):
 
 
 def main():
+    """Main function to run the factory tool"""
     global FORCE_STOP
 
     parser = ArgumentParser(prog='python3 factory.py',
@@ -152,12 +133,10 @@ def main():
                         help="Arduino port")
     args = parser.parse_args()
 
-    # TODO: HW2 Create a Queue
     q = Queue()
 
-    # TODO: HW2 Create thread_cam1 and thread_cam2 threads and start them.
-    t1 = threading.Thread(target=thread_cam1,args=(q,))
-    t2 = threading.Thread(target=thread_cam2,args=(q,))
+    t1 = threading.Thread(target=thread_cam1, args=(q,))
+    t2 = threading.Thread(target=thread_cam2, args=(q,))
 
     t1.start()
     t2.start()
@@ -167,23 +146,22 @@ def main():
             if cv2.waitKey(10) & 0xff == ord('q'):
                 break
 
-            # TODO: HW2 get an item from the queue. You might need to properly handle exceptions.
-            # de-queue name and data
-            name,data = q.get()
+            try:
+                name, data = q.get_nowait()
 
-            # TODO: HW2 show videos with titles of 'Cam1 live' and 'Cam2 live' respectively.
-            if "VIDEO:" in name:
-                imshow(name[6:],data)
+                if "VIDEO:" in name:
+                    imshow(name[6:], data)
 
-            # TODO: Control actuator, name == 'PUSH'
+                if name == 'PUSH':
+                    ctrl.push_actuator(data)
 
-            if name == 'PUSH':
-                ctrl.push_actuator(data)
+                if name == 'DONE':
+                    FORCE_STOP = True
 
-            if name == 'DONE':
-                FORCE_STOP = True
+                q.task_done()
 
-            q.task_done()
+            except Empty:
+                continue
 
     t1.join()
     t2.join()
@@ -194,5 +172,6 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-    except Exception:
-        os._exit()
+    except Exception as e:
+        print(f"Error: {e}")
+        os._exit(1)
--- class02/homework/yj_jung/hw3/README.md
+++ class02/homework/yj_jung/hw3/README.md
@@ -1,26 +1,19 @@
 # Homework03
 Smart factory 불량 분류모델 training 결과
 
-## Dataset 구조
-```
-(.otx)$ ds_count ./splitted_dataset 2
-./splitted_dataset/: 117
-./splitted_dataset/train: 93​
-./splitted_dataset/train/<Good>: 51​
-./splitted_dataset/train/<Bad>: 42​
-./splitted_dataset/val: 24
-./splitted_dataset/val/<Good>: 12​
-./splitted_dataset/val/<Bad>: 12​
-```
 
 ## Training 결과
 |Classification model|Accuracy|FPS|Training time|Batch size|Learning rate|Other hyper-prams|
 |----|----|----|----|----|----|----|
-|EfficientNet-V2-S| 1.0| 125| 0:00:08.279238| 64| 0.0071
-|EfficientNet-B0| 1.0| 125| 0:00:08.279238| 64| 0.0049|
-|DeiT-Tiny| 1.0| 125| 0:00:08.279238| 64| 0.0001|
-|MobileNet-V3-large-1x| 1.0| 142|'0:00:07.755650'| 64|0.0058|
+|DeiT-Tiny|1.0|3629|0.0033058400003938004|64|0.0001| | 
 
 
 ## FPS 측정 방법
-1000 / frameTime
+
+Optimization 후
+2024-06-17 16:10:45,348 | INFO : Avg time per image: 0.00027548666669948335 secs
+2024-06-17 16:10:45,348 | INFO : Total time: 0.0033058400003938004 secs
+2024-06-17 16:10:45,348 | INFO : Classification OpenVINO inference completed
+Performance(score: 1.0, dashboard: (3 metric groups))
+
+1 / Avg time per image로 계산
